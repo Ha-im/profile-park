a3e5a61b6c459addd06041a2f389d395fd34c623
Ha-im <gungsun1@naver.com>
2025-09-21 20:24:10 +0900

    🛠️ projects 이미지 hover css 수정중.

--- src/app/page.js
+++ src/app/page.js
@@ -333,12 +333,7 @@ export default function Home() {
             {filteredProjects.length > 0 ? (
               filteredProjects.map((project, idx) => (
                 <div className="project-card" key={idx}
-                  style={{
-                    '--hover-bg': `url(${project.imageUrl})`, // url() 포함하고 변수명 맞춤
-                    '--hover-opacity': 0
-                  }}
-                  onMouseEnter={(e) => e.currentTarget.style.setProperty('--hover-opacity', 0.2)}
-                  onMouseLeave={(e) => e.currentTarget.style.setProperty('--hover-opacity', 0)}
+                  style={{ '--hover-bg': `url(${project.imageUrl})` }}
                  >
                   <div>
                     <h5 className="project-card-title">{project.title}</h5>
